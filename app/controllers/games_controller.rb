2046c2eb1df1313e82770d13ec191e488a5d6c11
bkichler <brian.kichler@gmail.com>
2018-11-11 21:36:17 +0000

    Add auth to games controller, support devise methods in RSpec, add basic update method to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def new
     @game = Game.new
@@ -14,6 +15,10 @@ class GamesController < ApplicationController
   end
 
   def update
+    @game = Game.find_by_id(params[:id])
+    return render_not_found if @game.nil?
+    @game.update_attributes(game_params)
+    redirect_to game_path(@game.id)
   end
 
   private 
