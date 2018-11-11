87f414457053429353250760523c99eafb16a04d
bkichler <brian.kichler@gmail.com>
2018-11-11 21:03:16 +0000

    Add test for join method and factories to create test user and game

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-
+  describe 'games#update action' do
+    it 'should allow users to join games that have open seats' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = FactoryBot.create(:game)
+      patch :update, params: { id: game.id, game: { black_player_user_id: user.id } }
+      expect(response).to redirect_to game_path(game.id)
+      expect(game.black_player_user_id).to eq user.id
+    end
+  end
 end
