3c2c123f5eb36bc58b7c10c84ecaf1fc4bd95c65
Philip Vuong <philipvuong@yahoo.com>
2018-11-13 23:27:57 +0000

    valid move for rook and bishop

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,5 +1,10 @@
 class Rook < Piece
   def valid_move?(x_new, y_new)
-    true
+    rook_move = move_type(x_new, y_new)
+    if rook_move == :vertical || rook_move == :horizontal
+      !is_obstructed?(x_new, y_new) && within_bounds?(x_new, y_new)
+    else 
+      return false
+    end
   end
 end
