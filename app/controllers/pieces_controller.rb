8249ca90c8cc4a318de4a1165a8dd3265f0bbf67
bkichler <brian.kichler@gmail.com>
2018-12-01 19:33:05 +0000

    Add set_game! private method to piece controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,15 +1,21 @@
 class PiecesController < ApplicationController
+  before_action :set_game!
+
   def index
-    render json: Piece.order(:id)
+    render json: @game.pieces
   end
 
   def update
-    piece = Piece.find(params[:id])
+    piece = @game.pieces.find(params[:id])
     piece.update_attributes(piece_params)
     render json: piece
   end
 
   private
+
+  def set_game!
+    @game = Game.find(params[:game_id])
+  end
 
   def piece_params
     params.require(:piece).permit(:game_id, :user_id, :type, :color, :x_pos, :y_pos)
