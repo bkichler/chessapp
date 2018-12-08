1842fb3083c942d91dfe1cbe7485c4c1f4e4d31e
bkichler <brian.kichler@gmail.com>
2018-12-08 21:16:32 +0000

    Write test for piece controller move method

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -24,4 +24,36 @@ RSpec.describe PiecesController, type: :controller do
       expect(piece.x_pos).to eq(5)
     end
   end
+
+  describe 'pieces#move' do
+    it 'should call move_to! method and update piece position' do
+      user1 = FactoryBot.create(:user)
+      sign_in user1
+      user2 = FactoryBot.create(:user)
+      sign_in user2
+      game = FactoryBot.create(:game)
+
+      #Use game controller to join game
+      @controller = GamesController.new
+      put :join_as_white, params: { id: game.id, game: { white_player_user_id: user1.id } }
+      put :join_as_black, params: { id: game.id, game: { black_player_user_id: user2.id } }
+      game.reload
+
+      # Switch to piece controller, send put request to update method
+      @controller = PiecesController.new
+      piece = game.pieces.where("x_pos = ? AND y_pos = ?", 6, 4).first
+      put :move, params: { 
+                            game_id: game.id, 
+                            id: piece.id, 
+                            piece: { 
+                                      start_x: piece.x_pos, 
+                                      start_y: piece.y_pos,
+                                      end_x: 5,
+                                      end_y: 4
+                                    }
+                            }
+      piece.reload
+      expect(piece.x_pos).to eq(5)
+    end
+  end 
 end
